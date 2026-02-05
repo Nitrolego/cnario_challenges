4023e77a2d9c1a0f1b78e4d56351c7b88abcbaed
nitrolego <47659924+Nitrolego@users.noreply.github.com>
2026-02-05 17:50:07 +0800

    completed core test cases

--- test_cnarioeasy.py
+++ test_cnarioeasy.py
@@ -1,35 +1,95 @@
 import re
-from playwright.sync_api import Page, expect
+from playwright.sync_api import Playwright, sync_playwright, expect
 
-def test_empty_fields(page: Page):
+def test_empty_fields(playwright: Playwright):
+    #browser setup
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context()
+    page = context.new_page()
+
+    #go to page under test
     page.goto("https://www.cnarios.com/challenges/login-flow#challenge")
-    #setup: go to page
 
-    #test: input empty credentials
-    #assert: "Both username and password are required"
+    #test: click the login button with empty fields
+    page.get_by_role("button", name="Login").click()
 
-def test_invalid_credentials(page: Page):
-    #setup: go to page
+    #assert: error message is visible
+    expect(page.get_by_text("Both fields are required.")).to_be_visible()
+    
+    #clean up
+    context.close()
+    browser.close()
 
-    #test: input invalid credentials
-    #assert: "fail message"
-    print("test")
+def test_invalid_credentials(playwright: Playwright):
+    #browser setup
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context()
+    page = context.new_page()
 
-def test_login_as_user(page: Page):
-    #setup: go to page
+    #go to page under test
+    page.goto("https://www.cnarios.com/challenges/login-flow#challenge")
 
-    #test: input valid credentials as user
-    #assert: "You are logged in as USER"
-    print("test")
+    #test: click the login button with invalid credentials
+    page.get_by_role("textbox", name="Username").click()
+    page.get_by_role("textbox", name="Username").fill("wrongUser")
+    page.get_by_role("textbox", name="Username").press("Tab")
+    page.get_by_role("textbox", name="Password").fill("wrongPass")
+    page.get_by_role("button", name="Login").click()
 
-def test_login_as_admin(page: Page):
-    #setup: go to page
+    #assert: error message is visible
+    expect(page.get_by_text("Invalid username or password.")).to_be_visible()
+    
+    #clean up
+    context.close()
+    browser.close()
 
-    #test: input valid credentials as admin
-    #assert: "You are logged in as ADMIN"
-    print("test")
+def test_login_as_user(playwright: Playwright):
+    #browser setup
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context()
+    page = context.new_page()
 
-def test_logout(page: Page):
+    #go to page under test
+    page.goto("https://www.cnarios.com/challenges/login-flow#challenge")
+
+    #test: click the login button with user credentials
+    page.get_by_role("textbox", name="Username").click()
+    page.get_by_role("textbox", name="Username").fill("user")
+    page.get_by_role("textbox", name="Username").press("Tab")
+    page.get_by_role("textbox", name="Password").fill("user123")
+    page.get_by_role("button", name="Login").click()
+
+    #assert: error message is visible
+    expect(page.get_by_text("You are logged in as USER")).to_be_visible()
+    
+    #clean up
+    context.close()
+    browser.close()
+
+def test_login_as_admin(playwright: Playwright):
+    #browser setup
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context()
+    page = context.new_page()
+
+    #go to page under test
+    page.goto("https://www.cnarios.com/challenges/login-flow#challenge")
+
+    #test: click the login button with admin credentials
+    page.get_by_role("textbox", name="Username").click()
+    page.get_by_role("textbox", name="Username").fill("admin")
+    page.get_by_role("textbox", name="Username").press("Tab")
+    page.get_by_role("textbox", name="Password").fill("admin123")
+    page.get_by_role("button", name="Login").click()
+
+    #assert: error message is visible
+    expect(page.get_by_text("You are logged in as ADMIN")).to_be_visible()
+    
+    #clean up
+    context.close()
+    browser.close()
+
+def test_logout(playwright: Playwright):
     #setup, 
     # go to page
     # login as user/admin
