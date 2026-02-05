eb50ebafdcb51475b45645d3375792a949fce21f
nitrolego <47659924+Nitrolego@users.noreply.github.com>
2026-02-05 17:56:28 +0800

    added logout tests

--- test_cnarioeasy.py
+++ test_cnarioeasy.py
@@ -89,11 +89,54 @@ def test_login_as_admin(playwright: Playwright):
     context.close()
     browser.close()
 
-def test_logout(playwright: Playwright):
-    #setup, 
-    # go to page
-    # login as user/admin
+def test_logout_user(playwright: Playwright):
+    #browser setup
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context()
+    page = context.new_page()
 
-    #click logout button
-    #assert: "login form is visible again"
-    print("test")
+    #go to page under test
+    page.goto("https://www.cnarios.com/challenges/login-flow#challenge")
+
+    #setup: login as user
+    page.get_by_role("textbox", name="Username").click()
+    page.get_by_role("textbox", name="Username").fill("user")
+    page.get_by_role("textbox", name="Username").press("Tab")
+    page.get_by_role("textbox", name="Password").fill("user123")
+    page.get_by_role("button", name="Login").click()
+
+    #test: click the logout button
+    page.get_by_role("button", name="Logout").click()
+
+    #assert: error message is visible
+    expect(page.get_by_role("heading", name="Login", exact=True)).to_be_visible()
+    
+    #clean up
+    context.close()
+    browser.close()
+
+def test_logout_admin(playwright: Playwright):
+    #browser setup
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context()
+    page = context.new_page()
+
+    #go to page under test
+    page.goto("https://www.cnarios.com/challenges/login-flow#challenge")
+
+    #setup: login as admin
+    page.get_by_role("textbox", name="Username").click()
+    page.get_by_role("textbox", name="Username").fill("admin")
+    page.get_by_role("textbox", name="Username").press("Tab")
+    page.get_by_role("textbox", name="Password").fill("admin123")
+    page.get_by_role("button", name="Login").click()
+
+    #test: click the logout button
+    page.get_by_role("button", name="Logout").click()
+
+    #assert: error message is visible
+    expect(page.get_by_role("heading", name="Login", exact=True)).to_be_visible()
+    
+    #clean up
+    context.close()
+    browser.close()
